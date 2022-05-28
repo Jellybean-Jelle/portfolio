29d7cbfbed3e4206fb255d6ce5707db486691fff
Jelle Kuipers <Jelle30kuipers@gmail.com>
2022-05-28 22:46:17 +0200

    testing
    it works

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('layouts.inc.head')
+</head>
+<body class="bg-black-200 max-w-screen-sm">
+    <h1 class="bg-pink-500">Penis</h1>
+    @include('layouts.inc.header')
+
+    @yield('content')
+
+    @include('layouts.inc.footer')
+    @include('layouts.inc.script')
+</body>
+
+<footer>
+</footer>
+</html>
